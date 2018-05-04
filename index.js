122bd0970a042080235c917dcd14cc434a2405fa
Alex Bennett <benedictus.ab@gmail.com>
2018-05-04 09:14:00 -0600

    redux store

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+'use strict'
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import TabBarNavigation from './app/tabBar/views/TabBarNavigation';
+
+class Navigation extends React.Component {
+  render(){
+    return(
+      <Provider store={store}>
+        <TabBarNavigation />
+      </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent('Navigation', () => App);
